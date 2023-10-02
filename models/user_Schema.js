b8475c9b2f954ec7b344f2e54d66b5ebfcf302fa
Alman Butt <almanbutt6@outlook.com>
2023-10-02 16:22:11 +0500

    validation for login complete

--- models/user_Schema.js
+++ models/user_Schema.js
@@ -1,32 +1,33 @@
-const mongooose = require('mongoose');
+const mongooose = require("mongoose");
 
 const userSchema = new mongooose.Schema({
-    name: {
-        type: String,
-        required:true
-    },
-    email: {
-         type: String,
-        required:true
-    },
-    phone: {
-        type: Number,
-        required:true
-    },
-    work: {
-         type: String,
-        required:true
-    },
-    password: {
-         type: String,
-        required:true
-    },
-    cpassword: {
-         type: String,
-        required:true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  work: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cpassword: {
+    type: String,
+    required: true,
+  },
+});
 
-const User = mongooose.model('USER', userSchema);
+const User = mongooose.model("USER", userSchema);
 
 module.exports = User;
--- router/auth.js
+++ router/auth.js
@@ -1,14 +1,65 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user_Schema");
 
-router.get("/", (req, res) => {
-  res.send(`Hello world from the server rotuer js`);
+// Route for registration
+router.post("/register", async (req, res) => {
+  const { name, email, phone, work, password, cpassword } = req.body;
+
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    // If any field is missing, return an error response
+    return res.json({ error: "Please fill in all fields carefully" });
+  }
+
+  try {
+    // Check if a user with the same email already exists
+    const userExist = await User.findOne({ email: email });
+
+    if (userExist) {
+      return res.json({ message: "Email already exists" });
+    }
+
+    // Create a new user instance
+    const user = new User({ name, email, phone, work, password, cpassword });
+
+    // Save the user to the database
+    await user.save();
+
+    // Send a JSON response to the client with a success message
+    res.json({ message: "Registration successful" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-router.post("/register", (req, res) => {
+// Route for user login 
+router.post("/signin", async (req, res) => {
   console.log(req.body);
-   // Send a JSON response to the client with a message
-   res.json({ message: "Registration successful" });;
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({ error: "Invalid Data" });
+    }
+
+    // Check if a user with the provided email exists
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.json({ error: "User not found" });
+    }
+
+    // Check if the provided password matches the stored password
+    if (user.password !== password) {
+      return res.json({ error: "Invalid password" });
+    }
+
+    // If both checks pass, user login is successful
+    res.json({ message: "User sign-in successful" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
